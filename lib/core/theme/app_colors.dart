4d66eed7b41e8e85104a39ae102140de944ffdda
rmindra <indraaa.rm@gmail.com>
2026-05-02 13:44:09 +0700

    init (mobile): Folder Structure, Theme Colors, and Image Assets

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color primaryGreen = Color(0xFF006A65);
+  static const Color secondaryGreen = Color(0xFF20B2AA);
+  static const Color activeIcon = Color(0xFF0D9488);
+  static const Color textGreen = Color(0xFF003E3B);
+  static const Color textPrimary = Color(0xFF171D1C);
+  static const Color textSecondary = Color(0xFF3C4948);
+  static const Color grayForm = Color(0xFFE9EFED);
+  static const Color grayFormText = Color(0xFFBBC9C7);
+  static const Color grayIcon = Color(0xFF6C7A78);
+  static const Color background = Color(0xFFF5FBF9);
+  static const Color redButton = Color(0x66FFDAD6);
+  static const Color redButtonText = Color(0xFFBA1A1A);
+  static const Color redButtonBorder = Color(0x33BA1A1A);
+}
---
+++ lib/service/supabase_client.dart
@@ -0,0 +1,8 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class SupabaseConfig {
+  static const url = '';
+  static const anonKey = '';
+}
+
+final supabase = Supabase.instance.client;
