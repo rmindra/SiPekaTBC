a1d0c06c7756d1a51d04c5bbd63a2c90c04a51c3
rmindra <indraaa.rm@gmail.com>
2026-05-02 14:59:11 +0700

    feat (db): add supabase url and anonKey

--- lib/service/supabase_client.dart
+++ lib/service/supabase_client.dart
@@ -1,8 +1,8 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseConfig {
-  static const url = '';
-  static const anonKey = '';
+  static const url = 'https://dqzcksduehuiohbgheox.supabase.co';
+  static const anonKey = 'sb_publishable_hHX-xtEpiNuhPXvnOGh4dQ_8A8O3TFc';
 }
 
 final supabase = Supabase.instance.client;
